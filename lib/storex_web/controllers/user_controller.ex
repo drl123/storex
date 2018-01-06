a9a256407e95387f4339bac502d9a383fa1a1189
Don Lettrich <don.lettrich@gmail.com>
2018-01-05 20:45:00 -0500

    end ch11 sign up/sign in complete

--- lib/storex_web/controllers/user_controller.ex
+++ lib/storex_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule StorexWeb.UserController do
   use StorexWeb, :controller
   alias Storex.Accounts
+  alias StorexWeb.Plugs.CurrentUser
 
   def new(conn, params) do
     changeset = Accounts.new_user()
@@ -11,7 +12,8 @@ defmodule StorexWeb.UserController do
     case Accounts.create_user(user_params)  do
       {:ok, user} ->
         conn
-        |> redirect(to: cart_path(conn, :index))
+        |> CurrentUser.set(user)
+        |> redirect(to: cart_path(conn, :show))
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
